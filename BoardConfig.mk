732f559fd0f9790e6b58527b1c67b67088f7cefd
Pausa <pausatep@gmail.com>
2014-01-09 22:42:06 +0100

    First commit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,29 +1,165 @@
-USE_CAMERA_STUB := true
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# inherit from the proprietary version
--include vendor/htc/k2_ul/BoardConfigVendor.mk
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
 
-TARGET_ARCH := arm
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+
+BOARD_VENDOR := htc
+
+# Bootloader
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8230
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno305
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_CPU_SMP := true
+TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
 
-TARGET_BOOTLOADER_BOARD_NAME := k2_ul
+# Flags
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+COMMON_GLOBAL_CFLAGS += -DHTCLOG
+COMMON_GLOBAL_CFLAGS += -DNEW_ION_API=1
 
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 user_debug=31
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_QCOM_AUDIO_VARIANT := caf
+TARGET_QCOM_DISPLAY_VARIANT := caf
+TARGET_QCOM_MEDIA_VARIANT := caf
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+TARGET_USES_QCOM_COMPRESSED_AUDIO := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+
+# Camera
+USE_CAMERA_STUB := true
+TARGET_PROVIDES_CAMERA_HAL := false
+
+# Graphics
+USE_OPENGL_RENDERER := true
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_USES_ION := true
+BOARD_EGL_CFG := device/htc/k2_ul/configs/egl.cfg
+
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Power
+TARGET_PROVIDES_POWERHAL := true
+
+# Webkit
+ENABLE_WEBGL := true
+TARGET_FORCE_CPU_UPLOAD := true
+
+# Include path
+TARGET_SPECIFIC_HEADER_PATH := device/htc/k2_ul/include
+
+# Kernel
 BOARD_KERNEL_BASE := 0x80600000
 BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 user_debug=31
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x1608000
+#TARGET_KERNEL_SOURCE := kernel/htc/k2_ul
 
-# fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_FLASH_BLOCK_SIZE := 131072
+# Audio
+BOARD_USES_FLUENCE_INCALL := true
+BOARD_USES_SEPERATED_AUDIO_INPUT := true
 
-TARGET_PREBUILT_KERNEL := device/htc/k2_ul/kernel
+# Bluetooth
+BOARD_HAVE_BLUETOOTH_QCOM := true
+BLUETOOTH_HCI_USE_MCT := true
 
+# Camera
+USE_CAMERA_STUB := false
+TARGET_PROVIDES_CAMERA_HAL := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_BSP_CAMERA_ABI_HACK
+COMMON_GLOBAL_CFLAGS += -DMR0_CAMERA_BLOB
+COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
+COMMON_GLOBAL_CFLAGS += -DHTC_CAMERA_HARDWARE
+
+# GPS
+BOARD_HAVE_NEW_QC_GPS := true
+
+# Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+
+# Wifi
+BOARD_HAS_QCOM_WLAN := true
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_DRIVER_MODULE_NAME := prima_wlan
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/prima_wlan.ko"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := k2_ul
+
+# Kernel
+#TARGET_KERNEL_CONFIG := k2_ul_defconfig
+
+# Bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/k2_ul/bluetooth
+
+# Use libril in the device tree
+#BOARD_PROVIDES_LIBRIL := true
+
+# Filesystem
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00fffe00
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x54fffc00
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x49fffe00
+BOARD_FLASH_BLOCK_SIZE := 131072
+#BOARD_VOLD_MAX_PARTITIONS := 36
+
+# Vold
+#BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+#TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+
+# Recovery
+TARGET_PREBUILT_RECOVERY_KERNEL := device/htc/k2_ul/recovery/kernel
+
+# TWRP
+DEVICE_RESOLUTION := 540x960
+TW_FLASH_FROM_STORAGE := true
+BOARD_HAS_NO_REAL_SDCARD := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TW_INCLUDE_DUMLOCK := true
+TW_INCLUDE_JB_CRYPTO := true
+
--- cm.mk
+++ cm.mk
@@ -4,6 +4,10 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Release name
 PRODUCT_RELEASE_NAME := k2_ul
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 800
+TARGET_SCREEN_WIDTH := 480
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
@@ -14,5 +18,5 @@ $(call inherit-product, device/htc/k2_ul/device_k2_ul.mk)
 PRODUCT_DEVICE := k2_ul
 PRODUCT_NAME := cm_k2_ul
 PRODUCT_BRAND := htc
-PRODUCT_MODEL := k2_ul
-PRODUCT_MANUFACTURER := htc
+PRODUCT_MODEL := One SV
+PRODUCT_MANUFACTURER := HTC
